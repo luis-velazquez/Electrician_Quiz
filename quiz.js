81b85d2d32089fdbbdc60de440c4aceba7013913
Luis Velazquez <57099991+luis-velazquez@users.noreply.github.com>
2022-09-20 03:59:20 -0500

    Tuesday Changes

--- quiz.js
+++ quiz.js
@@ -11,7 +11,6 @@ const counter = document.getElementById("counter");
 const timeGauge = document.getElementById("timeGauge");
 const progress = document.getElementById("progress");
 const scoreDiv = document.getElementById("scoreContainer");
-// const qNumber = document.getElementById("qNumber").
 
 // create our questions
 let questions = [
@@ -127,32 +126,29 @@ let questions = [
         choiceC: "",
         choicedD: "None of the above",
         correct: "A"
+    }, {
+        question: "",
+        imgSrc: "",
+        choiceA: "", 
+        choiceB: "",
+        choiceC: "",
+        choiceD: ""
     }
     
 
 ];
 
 // create some variables
-
 const lastQuestion = questions.length - 1;
 let runningQuestion = 0;
 let count = 0;
-const questionTime = 20; //20px
 const gaugeWidth = 150; // 150px
-const gaugeUnit = gaugeWidth / questionTime;
-// let TIMER;
 let score = 0;
 
-//render question number 
-function renderNumber() {
-    let arrNumber = question.length
-    console.log(arrNumber)
-}
 
 // render a question
 function renderQuestion() {
     let q = questions[runningQuestion];
-
 
     question.innerHTML = "<p>" + q.question + "</p>";
     qImg.innerHTML = "<img src=" + q.imgSrc + ">";
@@ -181,29 +177,6 @@ function renderProgress() {
     }
 }
 
-// counter render
-
-// function renderCounter() {
-//     if (count <= questionTime) {
-//         counter.innerHTML = count;
-//         timeGauge.style.width = count * gaugeUnit + "px";
-//         count++
-//     } else {
-//         count = 0;
-//         // change progress color to red
-//         answerIsWrong();
-//         if (runningQuestion < lastQuestion) {
-//             runningQuestion++;
-//             renderQuestion();
-//         } else {
-//             // end the quiz and show the score
-//             clearInterval(TIMER);
-//             scoreRender();
-//         }
-//     }
-// }
-
-// checkAnwer
 
 function checkAnswer(answer) {
     if (answer == questions[runningQuestion].correct) {
